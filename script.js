f5f76bbcd7f9cf714ab7b548ef22d918bc9626fe
Mohammed Bin Yahiya <129117645+mx7h@users.noreply.github.com>
2025-08-26 10:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	
+	 let abc = new Blob([str],
+                { type: 'text/plain' });
+	return abc.size
+	
 };
 
 // Do not change the code below
